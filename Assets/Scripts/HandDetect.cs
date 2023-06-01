7756559f90f319c3abd6298d2d4be3f9073fe377
Kai <55585019+ThedragonZkai@users.noreply.github.com>
2023-06-01 11:46:43 +0200

    updated hand detect

--- Assets/Scripts/HandDetect.cs
+++ Assets/Scripts/HandDetect.cs
@@ -8,6 +8,7 @@ public class HandDetect : MonoBehaviour
 	bool isHolding = false;
 	GameObject holdingObj;
 	string side;
+	GameObject touchingObj;
 	// Start is called before the first frame update
 	void Start()
     {
@@ -21,24 +22,31 @@ public class HandDetect : MonoBehaviour
 	}
 
     // Update is called once per frame
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("entered trigger");
+		
+		if (other.transform.tag == "Interactable")
+		{
+			touchingObj = other.transform.gameObject;
+		}
+	}
+
     void Update()
     {
 		if (isHolding) {
 			holdingObj.transform.position = this.gameObject.transform.position;
 		}
 
-        if (Input.GetButtonDown("XRI_" + side + "_Trigger") || Input.GetKeyDown(KeyCode.Space)) {
-			Debug.Log("trigger pulled");
-		
-			void OnCollisionEnter(Collision other)
+			if (Input.GetButtonDown("XRI_" + side + "_Trigger") || Input.GetKeyDown(KeyCode.Space))
 			{
-				if (other.transform.tag == "Interactable")
-				{
-					holdingObj = other.gameObject;
-					isHolding = true;
-				}
+				Debug.Log("trigger pulled");
+			holdingObj = touchingObj;
+			isHolding = true;
+
 			}
-		}
+		
+
 		if (Input.GetButtonUp("XRI_" + side + "_Trigger") || Input.GetKeyUp(KeyCode.Space)) {
 			holdingObj = null;
 			isHolding = false;
